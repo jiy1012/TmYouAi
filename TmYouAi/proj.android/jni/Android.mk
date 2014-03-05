ce99cf72fa78a0c5b380177ffc35204feecdd9c2
jiy1012 <jiy1012@163.com>
2014-03-05 15:05:21 +0800

    新的Android.mk文件

--- TmYouAi/proj.android/jni/Android.mk
+++ TmYouAi/proj.android/jni/Android.mk
@@ -6,20 +6,25 @@ LOCAL_MODULE := game_shared
 
 LOCAL_MODULE_FILENAME := libgame
 
-LOCAL_SRC_FILES := hellocpp/main.cpp \
-                   ../../Classes/AppDelegate.cpp \
-                   ../../Classes/MainMenu.cpp \
-                   ../../Classes/Struct/TMHeroStruct.cpp \
-                   ../../Classes/MenuLayer.cpp \
-                   ../../Classes/HeaderLayer.cpp \
-                   ../../Classes/XmlConfig.cpp \
-                   ../../Classes/CCXMLParser.cpp 
-                   
-                   
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes                   
+define all-cpp-files
+$(patsubst jni/%,%, $(shell find $(LOCAL_PATH)/../../Classes/ $(LOCAL_PATH)/ -name "*.cpp"))  
+endef
+
+define all-cpp-dir
+	$(patsubst jni/%,$(LOCAL_PATH)/%, $(shell find $(LOCAL_PATH)/../../Classes/ -type d -not -wholename '*.svn*'))  
+endef
+
+LOCAL_SRC_FILES := $(call all-cpp-files)
+
+LOCAL_C_INCLUDES := $(call all-cpp-dir)
+
+#$(info $(LOCAL_SRC_FILES))
+#$(info $(LOCAL_C_INCLUDES))
+#include $(LOCAL_PATH)/Sources.mk
+#include $(LOCAL_PATH)/Includes.mk
 
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static cocosdenshion_static cocos_extension_static
-            
+
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,CocosDenshion/android) \
